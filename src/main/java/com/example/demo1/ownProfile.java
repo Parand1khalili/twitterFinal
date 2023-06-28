ef0825893f48e16692230d38ac8f96393d01600f
hossein goodarzi <hosseingoodarzi2003@gmail.com>
2023-06-28 17:37:25 +0330

    hossein 2 ownProfile buttuns

--- src/main/java/com/example/demo1/ownProfile.java
+++ src/main/java/com/example/demo1/ownProfile.java
@@ -1,4 +1,42 @@
 package com.example.demo1;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.image.ImageView;
+
 public class ownProfile {
+    @FXML
+    ImageView header;
+    @FXML
+    ImageView profPhoto;
+    @FXML
+    Label userId;
+    @FXML
+    Label bio;
+    @FXML
+    Label joinedDate;
+    @FXML
+    Label location;
+    @FXML
+    Label web;
+    @FXML
+    Button editProfile;
+    @FXML
+    Button followers;
+    @FXML
+    Button following;
+    @FXML
+    Tab tweets;
+    @FXML
+    Tab replies;
+    @FXML
+    Tab likes;
+    @FXML
+    Button timeLine;
+    @FXML
+    Button search;
+    @FXML
+    Button newTweet;
 }
