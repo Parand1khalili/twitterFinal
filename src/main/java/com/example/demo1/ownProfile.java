35e140b993a697d8133d642889c84c9fee94e720
hossein goodarzi <hosseingoodarzi2003@gmail.com>
2023-06-29 11:23:33 +0330

    hossein 2 ownProfile1

--- src/main/java/com/example/demo1/ownProfile.java
+++ src/main/java/com/example/demo1/ownProfile.java
@@ -1,12 +1,18 @@
 package com.example.demo1;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.image.ImageView;
 
-public class ownProfile {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class ownProfile implements Initializable {
     @FXML
     ImageView header;
     @FXML
@@ -39,6 +45,34 @@ public class ownProfile {
     Button search;
     @FXML
     Button newTweet;
-    
 
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Profile loggedUserProfile = null;
+        ArrayList<Tweet> loggedUserTweets;
+        try {
+            IO.out.writeObject("get-profile");
+            Thread.sleep(50);
+            IO.out.writeObject(logedUser.loggedUser);
+            Thread.sleep(50);
+            IO.out.writeObject(logedUser.loggedUser);
+            Thread.sleep(50);
+            loggedUserProfile = (Profile) IO.in.readObject();
+            loggedUserTweets = (ArrayList<Tweet>) IO.in.readObject();
+        } catch (IOException | InterruptedException | ClassNotFoundException e) {
+            // todo label error
+        }
+        if(loggedUserProfile != null) {
+            userId.setText(logedUser.loggedUser.getId());
+            bio.setText(loggedUserProfile.getBio());
+            location.setText(loggedUserProfile.getLocation());
+            web.setText(loggedUserProfile.getWeb());
+            followers.setText("followers " + loggedUserProfile.getFollowerNum());
+            following.setText("followings " + loggedUserProfile.getFollowingNum());
+            // todo set header
+            // todo set avatar
+
+        }
+    }
 }
