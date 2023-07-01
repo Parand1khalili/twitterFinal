2cedc57dffc4c4b7ee023f5884538ccfa1d4ccac
hossein goodarzi <hosseingoodarzi2003@gmail.com>
2023-07-02 00:11:08 +0330

    hossein 2 time-line final

--- src/main/java/com/example/demo1/timeLine.java
+++ src/main/java/com/example/demo1/timeLine.java
@@ -1,10 +1,16 @@
 package com.example.demo1;
 
 import common.Tweet;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -16,7 +22,6 @@ public class timeLine implements Initializable {
     @FXML
     ScrollPane scrollPane;
     VBox tweetContent;
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         tweetContent = new VBox();
@@ -42,5 +47,64 @@ public class timeLine implements Initializable {
             }
         }
     }
-
+    @FXML
+    ImageView profile;
+    @FXML
+    protected void onProfileClick(Event event){
+        ImageView imageView = (ImageView) event.getSource();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ownProfile.fxml"));
+        Parent root = null;
+        try {
+            root = fxmlLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Stage stage = (Stage) imageView.getScene().getWindow();
+        Scene scene = null;
+        if(root != null) {
+            scene = new Scene(root);
+        }
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    ImageView newTweet;
+    @FXML
+    protected void onNewTweetClick(Event event){
+        ImageView imageView = (ImageView) event.getSource();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("newTweet.fxml"));
+        Parent root = null;
+        try {
+            root = fxmlLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Stage stage = (Stage) imageView.getScene().getWindow();
+        Scene scene = null;
+        if(root != null) {
+            scene = new Scene(root);
+        }
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    ImageView search;
+    @FXML
+    protected void onSearchClick(Event event){
+        ImageView imageView = (ImageView) event.getSource();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("search.fxml"));
+        Parent root = null;
+        try {
+            root = fxmlLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Stage stage = (Stage) imageView.getScene().getWindow();
+        Scene scene = null;
+        if(root != null) {
+            scene = new Scene(root);
+        }
+        stage.setScene(scene);
+        stage.show();
+    }
 }
