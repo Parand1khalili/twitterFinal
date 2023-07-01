46f67837bbd4670ec63f39af8dfd308460fa2cdd
hossein goodarzi <hosseingoodarzi2003@gmail.com>
2023-07-01 22:55:46 +0330

    hossein 2 ViewTweet

--- src/main/java/com/example/demo1/timeLine.java
+++ src/main/java/com/example/demo1/timeLine.java
@@ -1,4 +1,27 @@
 package com.example.demo1;
 
-public class timeLine {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class timeLine implements Initializable {
+    @FXML
+    ScrollPane scrollPane;
+    @FXML
+    VBox tweetContent;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        tweetContent = new VBox();
+        scrollPane.setContent(tweetContent);
+        tweetContent.setSpacing(10);
+
+        tweetContent.setStyle("-fx-background-color:gray");
+
+    }
+
 }
